30c351c4f43e1ec5a051f9fa23799d20521633af
dbag <9499574@qq.com>
2018-11-29 13:35:20 +0800

    v1.1代码重构

--- README.md
+++ README.md
@@ -25,26 +25,24 @@ layui.config({
     //表格配置文件
     var tabConfig = {'page':true,'limits':[10,50,100],'height':400}
 
-    transfer.render({
+    var tb1 = transfer.render({
         elem: "#root", //指定元素
         cols: cols, //表格列  支持layui数据表格所有配置
         data: [data1,data2], //[左表数据,右表数据[非必填]]
         tabConfig: tabConfig //表格配置项 支持layui数据表格所有配置
     })
-
-
-    //transfer.get(参数1:类型,参数2:指定字段[选填])
-    //获取数据
+    
+    //transfer.get(参数1:初始化返回值,参数2:获取数据[all,left,right,l,r],参数:指定数据字段)
     $('.all').on('click',function () {
-        var data = transfer.get('all');
+        var data = transfer.get(tb1,'all');
         layer.msg(JSON.stringify(data))
     });
     $('.left').on('click',function () {
-        var data = transfer.get('l','id');
+        var data = transfer.get(tb1,'left','id');
         layer.msg(JSON.stringify(data))
     });
     $('.right').on('click',function () {
-        var data = transfer.get('r');
+        var data = transfer.get(tb1,'r');
         layer.msg(JSON.stringify(data))
     });
 })
@@ -52,3 +50,11 @@ layui.config({
 ```
 
 ### 问题反馈QQ群:  925487043 
+
+```html
+v1.1 2018年11月29日 13:32:57
+代码重构规范化
+```
+
+
+
--- layui_exts/transfer/transfer.js
+++ layui_exts/transfer/transfer.js
@@ -1,175 +1,199 @@
-/**
- * time: 2018年11月23日 17:28:32
- * author: dbag
- * blog: http://www.cnblogs.com/phper8/
- */
-layui.define('table', function(exports){
-  var $ = layui.$
-      ,table = layui.table
-   ,e = '' //elem id
-  ,c = [] //cols 表格列
-  ,d = [] //data 数据
-  ,id = [] //生成表格的ID
-  ,tc = [] //表格配置项
-  ,transfer = {
-  upData:function (data,field) {
-      //处理数据
-      if(field=='' && data && data.length > 0){
-          var d = [];
-          $.each(data,function (k,v) {
-              delete v.LAY_TABLE_INDEX
-              delete v.LAY_CHECKED
-              d.push(v)
-          })
-          return d
-      }else if(field && data && data.length > 0){
-          d = '';
-          $.each(data,function (k,v) {
-              if( k === 0 ){
-                  d += v[field]
-              }else{
-                  d += ','+ v[field]
-              }
-          })
-          return d
-      }else{
-          return [];
-      }
-  },
-  get:function (type,field='') {
-      //获取表格里面的数据
-      if(type==='all'){
-          var d1 = table.cache[id[0]],d2 = table.cache[id[1]];
-          return {lt:this.upData(d1,field),rt:this.upData(d2,field)}
-      }else if(type==='left' || type==='l'){
-          return this.upData(table.cache[id[0]],field)
-      }else if(type === 'right' || type==='r'){
-          return this.upData(table.cache[id[1]],field)
-      }
-  },
-    jth: function (id){
-      //计算中间按钮的中间位置
-        var h = $('#'+id).parent().height();
-        h =  h / 2 - 44;
-        $('#'+id).parents('.layui-row').find('.layui-col-md2').css('padding-top',h+'px') 
-    },
-    h: function(e){
-      //构建布局代码
-         var id1 = 'transfer_lt_'+Math.random().toString(36).substr(2);
-         var id2 = 'transfer_rt_'+Math.random().toString(36).substr(2);
-         var id3 = 'transfer_lb_'+Math.random().toString(36).substr(2);
-         var id4 = 'transfer_rb_'+Math.random().toString(36).substr(2);
-         var h = '<div class="layui-container">\
+layui.define('table',function (exports) {
+    "use strict";
+    var $ = layui.$
+        ,table = layui.table
+        ,MOD_NAME = 'transfer',LEFT_TABLE = 'left-table-',RIGHT_TABLE = 'right-table-',LEFT_BTN = 'left-btn-',RIGHT_BTN = 'right-btn-',FILTER= 'test'
+        ,DISABLED = 'layui-btn-disabled',BTN = 'button',BTN_STLY='btn',MD5='layui-col-md5',MD2='layui-col-md2'
+        ,transfer = {
+            index:layui.transfer?(layui.transfer+1000):0
+            ,idData:[] //ID池
+            ,update:function (data,filed) {
+                if(filed=='' && data && data.length > 0){
+                    var d = [];
+                    $.each(data,function (k,v) {
+                        delete v.LAY_TABLE_INDEX
+                        delete v.LAY_CHECKED
+                        d.push(v)
+                    })
+                    return d
+                }else if(filed && data && data.length > 0){
+                    d = '';
+                    $.each(data,function (k,v) {
+                        if( k === 0 ){
+                            d += v[filed]
+                        }else{
+                            d += ','+ v[filed]
+                        }
+                    })
+                    return d
+                }else{
+                    return [];
+                }
+            }
+            ,get:function (option,type,field='') {
+
+                var index = option.index
+                if(!index){
+                    return [];
+                }
+                if(type==='all'){
+                    var d = [];
+                    var d1 = table.cache[LEFT_TABLE+index],d2 = table.cache[RIGHT_TABLE+index];
+                    d1 = this.update(d1,field);
+                    d2 = this.update(d2,field)
+                    d.push({left:d1})
+                    d.push({right:d2})
+                    return d
+                }else if(type==='left' || type==='l'){
+                    return this.update(table.cache[LEFT_TABLE+index],field)
+                }else if(type === 'right' || type==='r'){
+                    return this.update(table.cache[RIGHT_TABLE+index],field)
+                }
+            }
+        }
+        ,thisRate = function () {
+            var that = this
+            return {
+                index:that.index
+            }
+        }
+        ,Class = function (options) {
+            var that = this
+            that.index = ++transfer.index
+            that.config = options
+            that.createHTMLDocument()
+            that.render()
+        };
+    Class.prototype.createHTMLDocument = function () {
+        var that = this
+            ,elem  = that.config.elem
+            ,index= that.index;
+        //创建页面元素
+        var html = '<div class="layui-container">\
                     <div class="layui-row">\
-                      <div class="layui-col-md5">\
-                        <table class="layui-hide" id="'+id1+'" lay-filter="test"></table>\
+                      <div class="'+MD5+'">\
+                        <table class="layui-hide" id="'+LEFT_TABLE+index+'" lay-filter="'+FILTER+'"></table>\
                       </div>\
-                      <div class="layui-col-md2" style="text-align: center;">\
-                        <div id="'+id3+'"  style="margin-bottom: 10px;"><button data-type="0" class="layui-btn  layui-btn-disabled btn"> <i class="layui-icon">&#xe602;</i></button></div>\
-                        <div id="'+id4+'" ><button data-type="1" class="layui-btn layui-btn-disabled btn"> <i class="layui-icon">&#xe603;</i></button></div>\
+                      <div class="'+MD2+'" style="text-align: center;">\
+                        <div id="'+LEFT_BTN+index+'"  style="margin-bottom: 10px;"><button data-type="0" data-index="'+index+'" class="layui-btn  '+DISABLED+' '+BTN_STLY+'"> <i class="layui-icon">&#xe602;</i></button></div>\
+                        <div id="'+RIGHT_BTN+index+'" ><button data-type="1" data-index="'+index+'" class="layui-btn '+DISABLED+' '+BTN_STLY+'"> <i class="layui-icon">&#xe603;</i></button></div>\
                       </div>\
-                      <div class="layui-col-md5">\
-                        <table class="layui-hide" id="'+id2+'" lay-filter="test"></table>\
+                      <div class="'+MD5+'">\
+                        <table class="layui-hide" id="'+RIGHT_TABLE+index+'" lay-filter="'+FILTER+'"></table>\
                       </div>\
                     </div>\
                   </div>';
-         $(e).html(h)
-        return [id1,id2,id3,id4];
-    },
-    csh: function(){
-      //生成表格
-        var d1 = d[0]?d[0]:[];
-        var d2 = d[1]?d[1]:[];
+        $(elem).html(html)
+    }
+    //初始化表格
+    Class.prototype.render = function () {
+        var that = this,options = that.config;
         var d1_c = {
-            elem: '#'+id[0]
-            ,cols: [c]
-            ,data: d1
-            ,id:id[0]
+            elem: '#'+LEFT_TABLE+that.index
+            ,cols: [options.cols]
+            ,data: (options.data[0]?options.data[0]:[])
+            ,id:LEFT_TABLE+that.index
         }
         var d2_c = {
-            elem: "#"+id[1]
-            ,cols: [c]
-            ,data: d2
-            ,id:id[1]
+            elem: '#'+RIGHT_TABLE+that.index
+            ,cols: [options.cols]
+            ,data: (options.data[1]?options.data[1]:[])
+            ,id:RIGHT_TABLE+that.index
         }
-        if(tc){
-            d1_c  = $.extend(d1_c,tc)
-            d2_c  = $.extend(d2_c,tc)
+        if(options.tabConfig){
+            d1_c = $.extend(d1_c,options.tabConfig)
+            d2_c = $.extend(d2_c,options.tabConfig)
         }
-        table.render(d1_c);
-        table.render(d2_c);
-    },
-  	render: function(p){
-      //初始化代码
-        if(!p || $(p.elem).length === 0){
-            console.log(p.elem+':不存在');
-            return false;
-        }
-        if(p.cols.length === 0){
-           console.log('field字段不存在');
-           return false; 
-        }
-         id = this.h(p.elem);
-         e = p.elem
-         c = p.cols
-         d = p.data
-         tc = p.tabConfig
-        this.csh()
-        this.jth(id[0])
+        transfer.idData.push(that.index)
+        table.render(d1_c)
+        table.init()
+        table.render(d2_c)
+
+        that.move()
+        that.click()
+    };
+    //左右移动按钮根据左表格居中
+    Class.prototype.move = function () {
+        var that = this
+            ,elem = $('#'+LEFT_TABLE+that.index)
+            ,h = elem.parent().height();
+        h =  h / 2 - 44;
+        elem.parents('.layui-row').find('.'+MD2).css('padding-top',h+'px')
     }
-  }
-  table.on('checkbox(test)', function(obj){
-     var checkStatus1 = table.checkStatus(id[0])
-      ,d1 = checkStatus1.data;
-      if(!d1.length){
-          $('#'+id[2]).children('button').addClass('layui-btn-disabled');
-      }else if(d1.length > 0){
-          $('#'+id[2]).children('button').removeClass('layui-btn-disabled');
-      }
-      var checkStatus2 = table.checkStatus(id[1])
-          ,d2 = checkStatus2.data;
-      if(!d2.length){
-          $('#'+id[3]).children('button').addClass('layui-btn-disabled');
-      }else if(d2.length > 0){
-          $('#'+id[3]).children('button').removeClass('layui-btn-disabled');
-      }
-  });
-  //数据左右移动 参数1 需要被删除的  参数2 需要被增加的  参数3 选中的数据  参数4 左还是右
-  function shiftData(d1,d2,dd,type){
-      var da = [];//未选中的数据
-      // d1.reverse();
-      $.each(d1,function(k,v){
-          if(!v.LAY_CHECKED){
-              da.push(v)
-          }
-      })
-      // dd.reverse();
-      $.each(dd,function(kk,vv){
-          d2.unshift(vv)
-      })
-      d = [];
-      if(type==0){
-          d.push(da)
-          d.push(d2)
-      }else if(type==1){
-          d.push(d2)
-          d.push(da)
-      }
-      transfer.csh()
-      $('.btn').addClass('layui-btn-disabled');
-  }
-  $(document).on('click','.btn',function(){
-      var type = $(this).data('type');
-      var checkStatus = table.checkStatus(id[type]),data = checkStatus.data;
-      var d1 = table.cache[id[0]]
-      var d2 = table.cache[id[1]]
-      if(type==0 && data){
-          //左边的数据移动到右表
-          shiftData(d1,d2,data,type)
-      }else if(type==1 && data){
-          //右表的数据移动到左表
-          shiftData(d2,d1,data,type)
-      }
-  })
-  exports('transfer',transfer);
-});
+    //点击事件
+    Class.prototype.click = function () {
+        var that = this,option = that.config;
+        $(option.elem).find('.'+BTN_STLY).each(function () {
+            var othis = $(this)
+            othis.on('click',function () {
+                if(!$(this).hasClass(DISABLED)){
+                    var type = $(this).data('type'),index = $(this).data('index');
+                    var checkStatus = table.checkStatus((type==0?LEFT_TABLE+index:RIGHT_TABLE+index)),data = checkStatus.data;
+                    var d1 = table.cache[LEFT_TABLE+index]
+                    var d2 = table.cache[RIGHT_TABLE+index]
+                    if(type==0 && data){
+                        //左边的数据移动到右表
+                        that.shiftData(d1,d2,data,type)
+                    }else if(type==1 && data){
+                        //右表的数据移动到左表
+                        that.shiftData(d2,d1,data,type)
+                    }
+                }
+            })
+        })
+    }
+    //数据处理
+    Class.prototype.shiftData = function (data1,data2,data,type) {
+        var da = [];//未选中的数据
+        // d1.reverse();
+        $.each(data1,function(k,v){
+            if(!v.LAY_CHECKED){
+                da.push(v)
+            }
+        })
+        // dd.reverse();
+        $.each(data,function(kk,vv){
+            data2.unshift(vv)
+        })
+        var d = [];
+        if(type==0){
+            d.push(da)
+            d.push(data2)
+            $('#'+LEFT_BTN+this.index).children(BTN).addClass(DISABLED);
+        }else if(type==1){
+            d.push(data2)
+            d.push(da)
+            $('#'+RIGHT_BTN+this.index).children(BTN).addClass(DISABLED);
+        }
+        this.config.data = d
+        this.render()
+    }
+    //选中状态
+    table.on('checkbox('+FILTER+')', function(obj){
+        var idData = transfer.idData,lenght = idData.length;
+        if(lenght > 0){
+            for (var i=0;i<=lenght-1;i++){
+                var checkStatus1 = table.checkStatus(LEFT_TABLE+idData[i])
+                    ,data_1 = checkStatus1.data
+                    ,checkStatus2 = table.checkStatus(RIGHT_TABLE+idData[i])
+                    ,data_2 = checkStatus2.data;
+                if(data_1.length >0){
+                    $('#'+LEFT_BTN+idData[i]).children(BTN).removeClass(DISABLED);
+                }else{
+                    $('#'+LEFT_BTN+idData[i]).children(BTN).addClass(DISABLED);
+                }
+                if(data_2.length >0){
+                    $('#'+RIGHT_BTN+idData[i]).children(BTN).removeClass(DISABLED);
+                }else{
+                    $('#'+RIGHT_BTN+idData[i]).children(BTN).addClass(DISABLED);
+                }
+            }
+        }
+    });
+
+    transfer.render = function (options) {
+        var inst = new Class(options)
+        return thisRate.call(inst)
+    }
+    exports(MOD_NAME,transfer)
+})
